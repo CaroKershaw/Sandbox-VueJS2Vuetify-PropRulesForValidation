51dcb481f86c00f9941f532818362861158bf7b8
Caroline Kershaw <75424622+CaroKershaw@users.noreply.github.com>
2022-08-23 15:50:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="app">
+    <v-container>
+      <MyForm />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import MyForm from "./components/MyForm";
+
+export default {
+  name: "App",
+  components: {
+    MyForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
